445f9d92d6c1b7b6e66e6332796d65734db57af4
daveying <daveying@163.com>
2020-12-28 23:24:42 +0800

    Add json parser code

---
+++ core/exception.hpp
@@ -0,0 +1,16 @@
+/*
+ * Auther: daveying@163.com
+ */
+
+#ifndef STACK_JSON_EXCEPTION_HPP
+#define STACK_JSON_EXCEPTION_HPP
+
+#include <exception>
+
+namespace stack_json
+{
+
+
+}; // namespace stck_json
+
+#endif // #define STACK_JSON_EXCEPTION_HPP
